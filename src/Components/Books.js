561c6c95a7bf0731b8b6afff647c42c583ef70cc
teitonny <tonnytei4@gmail.com>
2023-03-04 19:52:17 +0300

    Added the format for book website

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,13 +1,36 @@
 /* eslint-disable */
-import BooksCss from './Books.module.css'
+import BooksCss from './Books.module.css';
+
+const bookData = [
+    {
+        Genre: 'Action',
+        Title: 'The Hunger Games',
+        Author: 'Suzanne Collins',
+        percentage: '64%',
+    },
+    {
+        Genre: 'Science',
+        Title: 'Dune',
+        Author: 'Frank Herbert',
+        percentage: '8%',
+    },
+    {
+        Genre: 'Economy',
+        Title: 'Capital in Twenty ',
+        Author: 'Suzanne Collins',
+        percentage: '0%',
+    }
+]
 
 const Books =()=> {
     return (
-        <div className={BooksCss.bookContainer}>
+        <>
+        {bookData.map((bookInfo)=> (
+            <div className={BooksCss.bookContainer}>
             <div className={BooksCss.bookDetails}>
-                <div className={BooksCss.bookGenre}>Genre</div>
-                <div className={BooksCss.bookTitle}>Title</div>
-                <div className={BooksCss.bookAuthor}>Author</div>
+                <div className={BooksCss.bookGenre}> {bookInfo.Genre}</div>
+                <div className={BooksCss.bookTitle}> {bookInfo.Title}</div>
+                <div className={BooksCss.bookAuthor}> {bookInfo.Author}</div>
                 <div>
                     <ul className={BooksCss.bookActionList}>
                         <li className={BooksCss.bookAction}>Comment</li>
@@ -19,22 +42,31 @@ const Books =()=> {
 
             <div className={BooksCss.bookStatus}>
                 <div className={BooksCss.statusCircleContainer}>
-                    <div className={BooksCss.statusCircle}>
-                        circle
-                    </div>
+                    <div className={BooksCss.statusCircle}> </div>
                 </div>
                 <div className='statusPercentage'>
-                    <div className={BooksCss.Percentage}>100%</div>
+                    <div className={BooksCss.Percentage}>{bookInfo.percentage}</div>
                     <div className={BooksCss.Completed}>Completed</div>
                 </div>
             </div>
 
             <div className="bookProgress">
-                <div>current chapter</div>
-                <div>my current title</div>
-                <button>update progress</button>
+                <div className={BooksCss.currentChapter}>current chapter</div>
+                <div className={BooksCss.progressChapter}>chapter 17</div>
+                <button className={BooksCss.progressUpdate}>update progress</button>
             </div>
         </div>
+        ))}
+
+        <div>
+            <div>Add New Book</div>
+            <form className={BooksCss.formContainer}>
+                <input></input>
+                <input></input>
+                <button>Add Book</button>
+            </form>
+        </div>
+        </>
     )
 }
 
